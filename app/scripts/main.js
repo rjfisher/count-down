32caf0f4eccbff4db7189f9909e97475ecb68f7a
Rob Fisher <rfisher@sevone.com>
2017-09-14 15:06:07 -0400

    Background change w/ transitions

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,12 @@
 $('#countdown').countdown('2017/09/28', function(event) {
   $(this).html(event.strftime('%w week(s) %d day(s) <br /> %H:%M:%S'));
 });
+
+setInterval(function() {
+  // Time to change the background
+  var imgNum = Math.floor(Math.random() * 6) + 1;
+  var imgStr = "url(../images/main-bg-" + imgNum +".jpg)";
+
+  $('#wrapper').css('backgroundImage', imgStr);
+
+}, 30000);
